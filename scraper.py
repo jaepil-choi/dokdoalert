8ac45491d36c9438b938b8cdddab831579a1156d
jaepil-choi <chljeffreyz@gmail.com>
2021-09-26 18:46:32 +0900

    [FEAT] : Implement retry strategy to avoid timeout

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
 import notifier
 from typing import Union
 
@@ -13,6 +16,21 @@ class TicketChecker:
 
         self.notifier = notifier.TelegramNotification()
         
+        # Session with retry strategy
+        self.session = requests.session()
+        assert_status_hook = lambda response, *args, **kwargs: response.raise_for_status()
+        self.session.hooks['response'] = [assert_status_hook]
+
+        retry_strategy = Retry(
+            total=10,
+            status_forcelist=[413, 429, 500, 502, 503, 504],
+            method_whitelist=['GET'],
+            backoff_factor=2
+        )
+
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        self.session.mount('https://', adapter)
+        self.session.mount('http://', adapter)        
 
     def set_attrs(self, request_url, request_headers, request_data, reservation_url):
         self.request_url = request_url
@@ -21,7 +39,7 @@ class TicketChecker:
         self.reservation_url = reservation_url
 
     def get_data(self):
-        response = requests.post(self.request_url, data=self.request_data, headers=self.request_headers)
+        response = self.session.post(self.request_url, data=self.request_data, headers=self.request_headers)
         response_bs = bs(response.content, 'html.parser')
 
         return response_bs
